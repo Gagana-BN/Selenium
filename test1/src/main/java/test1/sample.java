3cec26923d8c3e40611af36dda0ac17c68b7c70c
Gagana BN <Gagana BN@LAPTOP-EGP3AER7>
2021-10-27 15:48:17 +0530

    commit sample

---
+++ test1/src/main/java/test1/sample.java
@@ -0,0 +1,12 @@
+package test1;
+import java.util.Scanner;
+public class sample {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+System.out.println("enter the name=");
+String name=sc.next();
+System.out.println("name is"+ name);
+	}
+
+}
